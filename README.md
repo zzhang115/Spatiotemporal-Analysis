f3035dc1b016168ce7bba83770314a9d8259ed97
Matthew Malensek <matt@malensek.net>
2017-10-25 09:39:19 -0700

    Add starter files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# P2-Template
+# Project 2 - Spatiotemporal Analysis with MapReduce
+
+This repository includes starter files and a sample directory structure. You are welcome to use it or come up with your own project structure.
+
+Project Specification: https://www.cs.usfca.edu/~mmalensek/courses/cs686/projects/project-2.html
+
+# Deliverables
+
+The project specification defines several questions that you will answer with MapReduce jobs. You should edit this document (README.md) with your answers as you find them, including figures, references, etc. This will also serve as a way of tracking your progress through the milestones.
+
+## Deliverable I
+
+Responses go here.
+
+## Deliverable II
+
+Responses go here.
---
+++ src/main/java/edu/usfca/cs/mr/wordcount/WordCountJob.java
@@ -0,0 +1,53 @@
+package edu.usfca.cs.mr.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * This is the main class. Hadoop will invoke the main method of this class.
+ */
+public class WordCountJob {
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            // Give the MapRed job a name. You'll see this name in the Yarn
+            // webapp.
+            Job job = Job.getInstance(conf, "word count job");
+            // Current class.
+            job.setJarByClass(WordCountJob.class);
+            // Mapper
+            job.setMapperClass(WordCountMapper.class);
+            // Combiner. We use the reducer as the combiner in this case.
+            job.setCombinerClass(WordCountReducer.class);
+            // Reducer
+            job.setReducerClass(WordCountReducer.class);
+            // Outputs from the Mapper.
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(IntWritable.class);
+            // Outputs from Reducer. It is sufficient to set only the following
+            // two properties if the Mapper and Reducer has same key and value
+            // types. It is set separately for elaboration.
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+            // path to input in HDFS
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            // path to output in HDFS
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            // Block until the job is completed.
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
