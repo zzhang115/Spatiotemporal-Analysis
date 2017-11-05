3c81f3e35073db30617cc1ef7894b2e68ca400bc
zzhang115 <zzhang115@dons.usfca.edu>
2017-11-04 18:48:15 -0700

    add topk

--- src/main/java/edu/usfca/cs/mr/warmup/HotestTempJob.java
+++ src/main/java/edu/usfca/cs/mr/warmup/HotestTempJob.java
@@ -3,22 +3,62 @@ package edu.usfca.cs.mr.warmup;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.*;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.logging.Logger;
 
 /**
  * Created by zzc on 11/3/17.
  */
+
 public class HotestTempJob {
-    final static Logger logger = Logger.getLogger("SnowDepthJob");
-    final static int TOPK = 10;
+    final static Logger logger = Logger.getLogger("HotestTempJob");
+
+    public static class HotestTempMapper extends Mapper<LongWritable, Text, Text, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            System.out.println(value.toString());
+            String timeStamp = value.toString().split("\\s+")[0];
+            String geoHash = value.toString().split("\\s+")[1];
+            String temp = value.toString().split("\\s+")[40];
+            System.out.println(temp);
+            context.write(new Text("Temp"), new Text(timeStamp + "-" + geoHash + "-" + temp));
+        }
+    }
+
+    public static class HotestTempReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            Iterator<Text> iterator = values.iterator();
+            double maxTemp = 0.0;
+            String geoHash = "";
+            String timeStamp = "";
+            while (iterator.hasNext()) {
+                String value = iterator.next().toString();
+                double temp = Double.parseDouble(value.split("-")[2]);
+                String timestamp = value.split("-")[0];
+                String geohash = value.split("-")[1];
+                if (temp > maxTemp) {
+                    maxTemp = temp;
+                    timeStamp = timestamp;
+                    geoHash = geohash;
+                }
+            }
+            System.out.println(geoHash + "-" + timeStamp + "-" + maxTemp);
+            context.write(new Text(geoHash + "-" + timeStamp), new Text(String.valueOf(maxTemp)));
+        }
+    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
           try {
@@ -29,19 +69,19 @@ public class HotestTempJob {
             // Current class.
             job.setJarByClass(HotestTempJob.class);
             // Mapper
-            job.setMapperClass(SnowDepthMapper.class);
+            job.setMapperClass(HotestTempMapper.class);
             // Combiner. We use the reducer as the combiner in this case.
-            job.setCombinerClass(SnowDepthReducer.class);
+            job.setCombinerClass(HotestTempReducer.class);
             // Reducer
-            job.setReducerClass(SnowDepthReducer.class);
+            job.setReducerClass(HotestTempReducer.class);
             // Outputs from the Mapper.
             job.setMapOutputKeyClass(Text.class);
-            job.setMapOutputValueClass(DoubleWritable.class);
+            job.setMapOutputValueClass(Text.class);
             // Outputs from Reducer. It is sufficient to set only the following
             // two properties if the Mapper and Reducer has same key and value
             // types. It is set separately for elaboration.
             job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(DoubleWritable.class);
+            job.setOutputValueClass(Text.class);
             // path to input in HDFS
             FileInputFormat.addInputPath(job, new Path(args[0]));
             // path to output in HDFS
@@ -49,7 +89,6 @@ public class HotestTempJob {
             // Block until the job is completed.
             job.waitForCompletion(true);
 
-            sortSnowDepth(new File(args[1]), args[2] + "/topsnowdepth");
             System.exit(0);
         } catch (IOException e) {
             System.err.println(e.getMessage());
@@ -57,69 +96,6 @@ public class HotestTempJob {
             System.err.println(e.getMessage());
         } catch (ClassNotFoundException e) {
             System.err.println(e.getMessage());
-        }
-    }
-
-    private static class NodeComparator implements Comparator<Node> {
-        public int compare(Node a, Node b) {
-            if (a.snowDepth > b.snowDepth) {
-                return 1;
-            } else if (a.snowDepth < b.snowDepth) {
-                return -1;
-            } else {
-                return 0;
-            }
-        }
-    }
-    private static class Node {
-        String geoHash;
-        Double snowDepth;
-        Node(String geoHash, Double snowDepth) {
-            this.geoHash = geoHash;
-            this.snowDepth = snowDepth;
-        }
-    }
-
-    public static void sortSnowDepth(File dir, String filename) {
-    PriorityQueue<Node> queue = new PriorityQueue<Node>(TOPK, new NodeComparator());
-        if (dir.isDirectory()) {
-            for (File file : dir.listFiles()) {
-                if (!file.getName().startsWith("part")) {
-                    continue;
-                }
-                try {
-                    BufferedReader fileReader = new BufferedReader(new FileReader(file));
-                    String line;
-                    while ((line = fileReader.readLine()) != null) {
-                        String geoHash = line.split("\\s+")[0];
-                        Double snowDepth = Double.parseDouble(line.split("\\s+")[1]);
-                        queue.offer(new Node(geoHash, snowDepth));
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        writeResultToFile(filename, queue);
-    }
-
-    public static void writeResultToFile(String fileName, PriorityQueue<Node> queue) {
-        try {
-            FileWriter fileWriter = new FileWriter(fileName);
-            int i = 0;
-            logger.info("size: " + queue.size());
-            while (i < 10 && !queue.isEmpty()) {
-                Node node = queue.poll();
-                fileWriter.write(node.geoHash + ":" + node.snowDepth + "\n");
-                i++;
-            }
-
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
